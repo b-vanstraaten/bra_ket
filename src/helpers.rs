6f58c831e5b5a86c912064629ee015712acc3be1
sebastianorbell <sebastian.o@btinternet.com>
2022-03-17 08:53:01 +0000

    drawing gates

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,28 +1,79 @@
-use crate::{Program};
+use crate::{Program, Qubit};
+use crate::{gates::Gate};
 
 pub fn draw_circuit(
     program: &Program
 ) {
+    let mut v: Vec<String> = vec!["Qubit ".to_owned(); program.state.number_of_qubits];
     for n in 0..program.state.number_of_qubits {
-        for gate in program.gates {
-            println!("{} : ----{}", n, );
+        v[n].push_str(format!("{} : ---", n).as_str())
+    }
+
+    for gate in &program.gates {
+        for n in 0..program.state.number_of_qubits{
+            let m: String = plot_gate(gate, n) + "---";
+            v[n].push_str(&m);
+        }
+    }
+    println!("_____________________________");
+    println!("****** Quantum Circuit ******");
+    println!("");
+    for line in v {
+        println!("{:#?}", line);
+    }
+    println!("");
+    println!("*****************************");
+    println!("_____________________________");
+
+}
+//
+fn plot_gate( gate: &Gate, qubit_index: Qubit) -> String {
+    match gate {
+        Gate::Measure(qubit) => {
+            let m: String = "M------".to_owned();
+            return_string(qubit_index, *qubit, m)
+            },
+
+        Gate::X(qubit) => {
+            let m: String = "X------".to_owned();
+            return_string(qubit_index, *qubit, m)
+        },
+        Gate::Y(qubit) => {
+            let m: String = "Y------".to_owned();
+            return_string(qubit_index, *qubit, m)
+        },
+        Gate::Z(qubit) => {
+            let m: String = "Z------".to_owned();
+            return_string(qubit_index, *qubit, m)
+        },
+
+        Gate::RX(qubit, angle) => {
+            let m: String = format!("RX({:.*})", 1, angle);
+            return_string(qubit_index, *qubit, m)
+        },
+        Gate::RY(qubit, angle) => {
+            let m: String = format!("RY({:.*})", 1, angle);
+            return_string(qubit_index, *qubit, m)
+        },
+        Gate::RZ(qubit, angle) => {
+            let m: String = format!("RZ({:.*})", 1, angle);
+            return_string(qubit_index, *qubit, m)
+        },
+        Gate::H(qubit) => {
+            let m: String = "H------".to_owned();
+            return_string(qubit_index, *qubit, m)
+        },
+        _ => {
+            let default: String = "G------".to_owned();
+            default
         }
     }
 }
 
-pub fn plot_gate( gate: &Gate) {
-    debug!("{:?}", gate);
-    match gate {
-        Gate::Measure(qubit) => measure(state, qubit),
-
-        Gate::X(qubit) => x(state, qubit),
-        Gate::Y(qubit) => y(state, qubit),
-        Gate::Z(qubit) => z(state, qubit),
-
-        Gate::RX(qubit, angle) => rx(state, qubit, angle),
-        Gate::RY(qubit, angle) => ry(state, qubit, angle),
-        Gate::RZ(qubit, angle) => rz(state, qubit, angle),
-        Gate::H(qubit) => h(state, qubit),
-        Gate::CNOT(control, target) => cnot(state, control, target),
+fn return_string(indexed_qubit: Qubit, gate_qubit: Qubit, message: String) -> String {
+    if indexed_qubit == gate_qubit {
+        return message
     }
+    let default: String = "-------".to_owned();
+    return  default
 }
