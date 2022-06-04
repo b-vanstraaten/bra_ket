4e1e2723b76732445ccdf1e9f887ec914c7c5a1e
b-vanstraaten <69204219+b-vanstraaten@users.noreply.github.com>
2022-06-04 13:22:35 +0100

    restructure

--- examples/vqe.rs
+++ examples/vqe.rs
@@ -3,6 +3,7 @@ use nalgebra::matrix;
 use bra_ket::*;
 ///https://joshuagoings.com/2020/08/20/VQE/
 
+/// makes the ansatz circuit to prepare the quantum state
 fn make_ansatz(theta: Angle) -> Program {
     let mut ansatz = Program::new();
     ansatz.reinitialise_all();
@@ -20,6 +21,7 @@ fn make_ansatz(theta: Angle) -> Program {
     ansatz
 }
 
+/// makes the measurement circuits which permit measuring the pauli string in the hamiltonian.
 fn make_measurement_programs() -> Vec<Program> {
     let mut measure_z0 = Program::new();
 
@@ -43,23 +45,24 @@ fn make_measurement_programs() -> Vec<Program> {
     vec![measure_z0, measure_z1, measure_z0_z1, measure_x0_x1, measure_y0_y1]
 }
 
-fn main() {
-    let evaluate_energy = |theta| {
-        let ansatz = make_ansatz(theta);
-        let measurement_programs = make_measurement_programs();
-        let coefficients = vec![0.3435, -0.4347, 0.5716, 0.0910, 0.0910];
-        let mut state = StateVector::new(2);
+/// calculates the energy of the ansatz state for a given theta in the ansatz circuit.
+fn evaluate_energy(theta: Real) -> Real {
+    let ansatz = make_ansatz(theta);
+    let measurement_programs = make_measurement_programs();
+    let coefficients = vec![0.3435, -0.4347, 0.5716, 0.0910, 0.0910];
 
-        let mut energy = 0.7055696146 - 0.4804;
-        for (coefficient, measurement_program) in zip(coefficients, measurement_programs) {
+    let mut state = StateVector::new(2);
+    let expectation: Real = zip(coefficients, measurement_programs).map(
+        |(coefficient, measurement_program)| {
             let full_program = ansatz.to_owned() + measurement_program;
             full_program.run(&mut state);
-            let expectation = state.get_expectation(&0);
-            energy += coefficient * expectation;
+            coefficient * state.get_expectation(&0)
         }
-        energy
-    };
+    ).sum();
+    expectation + 0.7055696146 - 0.4804
+}
 
+fn main() {
     println!("{}", evaluate_energy(0.11))
 
 }
